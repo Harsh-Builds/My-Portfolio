8e2cb321576ca941008f9cef3a579d22a599cbe7
Harsh-Builds <harshsaini888555@gmail.com>
2025-09-11 21:56:51 +0530

    add more data on qualification section

--- client/src/components/qualification.js
+++ client/src/components/qualification.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGraduationCap, faAward } from "@fortawesome/free-solid-svg-icons";
 
 export default function Qualification() {
   return (
@@ -12,14 +15,16 @@ export default function Qualification() {
           className="qualification_button button--flex qualification_active"
           data-target="#education"
         >
-          <i className="fa-solid fa-graduation-cap qualification_icon" />
+            <FontAwesomeIcon icon={faGraduationCap} className='qualification_icon' />
+          
           Education
         </div>
         <div
           className="qualification_button button--flex "
           data-target="#certificates"
         >
-          <i className="fa-solid fa-award qualification_icon" />
+            <FontAwesomeIcon icon={faAward} className='qualification_icon' />
+       
           Certificates
         </div>
       </div>
@@ -77,6 +82,48 @@ export default function Qualification() {
           </div>
         </div>
        
+               {/*==================== QUALIFICATION Content 2 ====================*/}
+        <div
+          className="qualification_content"
+          data-content=""
+          id="certificates"
+        >
+          {/*==================== QUALIFICATION 1 ====================*/}
+          <div className="qualification_data">
+            <div>
+              <h3 className="qualification_title">
+                Accenture Developer &amp; Technology Virtual Experience
+              </h3>
+              <span className="qualification_subtitle">
+                Accenture Virtual Experience Program
+              </span>
+              <div className="qualification_calendar">
+                <i className="fa-solid fa-calendar-days" />
+                Nov 2024 – Dec 2024
+              </div>
+            </div>
+            <div>
+              <span className="qualification_rounder" />
+              <span className="qualification_line" />
+            </div>
+          </div>
+          {/*==================== QUALIFICATION 2 ====================*/}
+          <div className="qualification_data">
+            <div />
+            <div>
+              <span className="qualification_rounder" />
+              {/* <span class="qualification_line"></span> */}
+            </div>
+            <div>
+              <h3 className="qualification_title">
+                Goldman Sachs Operations Virtual Experience
+              </h3>
+              <span className="qualification_subtitle">
+                Operations Job Simulation Certificate
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
