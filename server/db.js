d224c68c981ca7cbde2d3bac032da3f8d47c1dc3
Harsh-Builds <harshsaini888555@gmail.com>
2025-09-12 23:22:35 +0530

    create the connection between server to database in db.js file

---
+++ server/db.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');  //here we import the mongoose library.
+require('dotenv').config();  // we need to import dotenv also here so we can process it.
+
+//Define the mongodb connection URL
+const mongoURL = 'mongodb://127.0.0.1:27017/myPortfolio';
+
+mongoose.connect(mongoURL, {
+    // useNewUrlParser : true,     // these both are just mandatory parameter to prevent future problems during database connection.
+    // useUnifiedTopology: true         // in this version they are not workable.
+})
+
+const theDB = mongoose.connection;
+
+theDB.on('connected', () => {        
+    console.log('Connected to MongoDB server');
+})
+
+theDB.on('error', (err) => {        
+    console.log('Mongo connection error', err);
+})
+
+theDB.on('disconnected', () => {        
+    console.log('DisConnected to MongoDB server');
+})
+
+// Now, Export the DataBase connection.
+module.exports = theDB;
